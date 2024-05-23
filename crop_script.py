57dc1364f797d37aaab5e18b9141fe1b08e2ed9a
Piter Nicolaas <piternicolaas@gmail.com>
2024-05-23 15:28:05 +0200

    added climax scene script first attempt

---
+++ crop_script.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def climax_scene(scenes):
+    scenes_counts = []
+    for scene in scenes:
+        charactercount = 0
+        scene.split()
+        for w in scene:
+            if friendsname(w):
+                charactercount +=1
+        scenes_counts.append(charactercount)
+    
+    max_index = 0
+    max_count = 0
+    for idx,count in enumerate(scenes_counts):
+        if count > max_count:
+            max_index = idx
+            max_count = count
+    return scenes[max_index]
+
+def friendsname(w):
+    friends = ['Phoebe', 'Chandler', 'Ross', 'Monica', 'Rachel', 'Joey', 'Geller', 'Pheebs']
+    for friend in friends:
+        if w.startswith(friend):
+            return 1
