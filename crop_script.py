355ef84647aa61681768fd957eba9fcb4c35b597
caspardj23 <caspardejong2003@icloud.com>
2024-05-24 17:17:30 +0200

    added surnames of the characters

--- crop_script.py
+++ crop_script.py
@@ -1,32 +1,47 @@
 import pandas as pd
 
+
 def climax_scene(scenes):
     scenes_counts = []
     for scene in scenes:
         charactercount = 0
         for w in scene:
             if friendsname(w) and len(w) >= 4:
-                print('1')
-                charactercount +=1
+                print("1")
+                charactercount += 1
         scenes_counts.append(charactercount)
-    
+
     max_index = 0
     max_count = 0
-    for idx,count in enumerate(scenes_counts):
+    for idx, count in enumerate(scenes_counts):
         if count > max_count:
             max_index = idx
             max_count = count
     return scenes[max_index]
 
+
 def friendsname(w):
-    friends = ['Phoebe', 'Chandler', 'Ross', 'Monica', 'Rachel', 'Joey', 'Geller', 'Pheebs']
+    friends = [
+        "Phoebe",
+        "Chandler",
+        "Ross",
+        "Monica",
+        "Rachel",
+        "Joey",
+        "Geller",
+        "Pheebs",
+        "Bing",
+        "Tribbiani",
+        "Buffay",
+    ]
     for friend in friends:
         if w.startswith(friend):
             return True
-        
-# search through episodes to find where in episodes scene with most names occurs. 
+
+
+# search through episodes to find where in episodes scene with most names occurs.
 # could return percentage or 'Scene 20/45 has the most occurences of names'.
-        
+
 # for episode in episodes:
 #        word_count = len(episode)
 #        climax_scene(episodes)
